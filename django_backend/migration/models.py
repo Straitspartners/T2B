d5174306835514bee1287af12d5b5062b4ee6670
Kavyaanjali2110 <kavyaanjali2707@gmail.com>
2026-06-02 15:17:50 +0530

    Added columns to database

--- django_backend/migration/models.py
+++ django_backend/migration/models.py
@@ -7,6 +7,12 @@ class AppUser(models.Model):
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    zoho_access_token = models.TextField(null=True, blank=True)
+    zoho_client_id = models.CharField(max_length=255, null=True, blank=True)
+    zoho_client_secret = models.CharField(max_length=255, null=True, blank=True)
+    zoho_organization_id = models.CharField(max_length=100, null=True, blank=True)
+    zoho_refresh_token = models.TextField(null=True, blank=True)
+
     def __str__(self):
         return self.email
 
